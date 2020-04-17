01b3908eefe0b0473841cd366d85595c995aa650
Megan Slawson <meg.slawson@gmail.com>
2020-04-17 04:46:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,7 +1,11 @@
 function basicTeenager(age) {
 if (age === 13) {
+<<<<<<< HEAD
   return "You are a teenager!"
 }
+=======
+  return "You are a teenager!" }
+>>>>>>> 0bf21ec90180358227d4906015b3956e93060f96
 }
 
 function teenager(age) {
@@ -15,23 +19,38 @@ function ageChecker(age) {
 if (age === 13){
   return "You are a teenager!"
 }
+<<<<<<< HEAD
 if (age === 29){
   return "You are a grownup"
 }
 if (age === 12){
   return "You are a kid"
 }
+=======
+if else (age === 12){
+return "You are a kid"
+>>>>>>> 0bf21ec90180358227d4906015b3956e93060f96
 }
 
+}
 function ternaryTeenager(age) {
+<<<<<<< HEAD
 if (age === 15){
   return "You are a teenager"
 }
   return "You are not a teenager"
+=======
+if (age === 13){
+  return "You are a teenager"
+}
+>>>>>>> 0bf21ec90180358227d4906015b3956e93060f96
 }
 function switchAge(age) {
 if (age === 15){
   return "You are a teenager"
 }
+<<<<<<< HEAD
   return "You have an age"
+=======
+>>>>>>> 0bf21ec90180358227d4906015b3956e93060f96
 }
